5aee1c975021c75c718449462ad60e7b8ad6fb12
Shawn <185832431+Shtealc@users.noreply.github.com>
2026-09-23 18:19:22 -0400

    changed naming convention for simplicity

--- DataRush/DataRush/DataRush/DataRush/Assets/Scripts/TestDebuggingScript.cs
+++ DataRush/DataRush/DataRush/DataRush/Assets/Scripts/TestDebuggingScript.cs
@@ -6,13 +6,13 @@ public class TestDebuggingScript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
-    void OnTriggerEnter(Collider other)
+    void OnCollisionEnter(Collision other)
     {
-        Debug.Log(gameObject.name + " Collider entered " + other.gameObject.name);
+       Debug.Log(gameObject.name + " Collision detected with: " + other.gameObject.name);
 
     }
 }
---
+++ DataRush/DataRush/DataRush/DataRush/Assets/Scripts/testForDebugLog.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class testForDebugLog: MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Test script is running.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
