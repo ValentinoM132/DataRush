47d98e0df66221f3d801cd71895ccb2efc173b16
Shawn <185832431+Shtealc@users.noreply.github.com>
2026-09-23 17:53:54 -0400

    added colliders to pipe prefab(meant to be replaced just for character testing purposes)

---
+++ DataRush/DataRush/DataRush/DataRush/Assets/Scripts/TestDebuggingScript.cs
@@ -0,0 +1,18 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class TestDebuggingScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Trigger entered");
+
+    }
+}
